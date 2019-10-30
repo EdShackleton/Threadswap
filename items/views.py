865b48de2c70dc2e69878afd1bce1423fb13d539
Ubuntu <ubuntu@ip-172-31-89-95.ec2.internal>
2019-10-30 19:17:50 +0000

    added itemdetail.html

--- items/views.py
+++ items/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Item
+from .forms import NewItemForm
 
-# Create your views here.
+def get_items(request):
+    """Create a view that will return a list of all items and render them
+    to the 'items.html' template"""
+    
+    items = Item.objects.filter(date_added__lte=timezone.now
+        ()).order_by('-date_added')
+    return render(request, "items.html", {"items" : items})
+
+def item_detail(request, pk):
+    """ Creates a view that shows a specific item based on the item ID (pk)
+    and render it to the 'itemdetail.html' template, 
+    or return a 404 error if not found"""
+    
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, "itemdetail.html", {"item" : item})
+
+def create_or_edit_item(request, pk=None):
+    """ Creates a view that allows us to create or edit an item
+    if the post ID is null or not"""
+    
+    item = get_object_or_404(Item, pk=pk) if pk else None
+    if request.method == "POST":
+        form = NewItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            item = form.save()
+            return redirect(item_detail, item.pk)
+    else:
+        form = NewItemForm(instance=item)
+    return render(request, 'newitemform.html', {'form' : form})
--- threadswap/urls.py
+++ threadswap/urls.py
@@ -13,15 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from accounts.views import index, logout, login, registration, user_profile
+from django.views.generic import RedirectView
+from django.views.static import serve
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name='index'),
+    url(r'^$', RedirectView.as_view(url='items/')),
     url(r'^accounts/logout/$', logout, name='logout'),
     url(r'^accounts/login/$', login, name='login'),
     url(r'^accounts/register/$', registration, name='registration'),
     url(r'^accounts/profile/$', user_profile, name='profile'),
+    url(r'items/', include('items.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root' : MEDIA_ROOT }),
 ]
