01fb954877d63eb189d1c5d43cb67fc77aa906e1
Ubuntu <ubuntu@ip-172-31-89-95.ec2.internal>
2020-01-21 01:52:34 +0000

    deploying

--- threadswap/settings.py
+++ threadswap/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['1eeef621679645c88e0fab3543262516.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = ['1eeef621679645c88e0fab3543262516.vfs.cloud9.us-east-1.amazonaws.com', 'threadswap-ecrs.herokuapp.com']
 
 
 # Application definition
